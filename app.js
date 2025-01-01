19c4b296519bd815e7f4a9172c949f708990965a
krishnendhu-ps <krishnendhups91@gmail.com>
2025-01-02 00:26:08 +0530

    Path corrected

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'view','lib.html'));
 });
 
-const PORT=4002;
+const PORT=process.env.PORT||4002;
 app.listen(PORT,()=>{
-console.log(`server is running on http://localhost:${PORT}`);
+console.log(`server is running on ${PORT}`);
 });
 
